657db9cd77ce0e6bf4bd6083d5c0bdaf0074e996
MatteoSanson <matteo.sanson@gmail.com>
2023-11-22 20:35:14 +0100

    feat: array oggetti snack2

--- snack2/main.js
+++ snack2/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-console.log('sto funzionando');
+const soccerTeam = [
+    {nameTeam:'Juventus', points: 0, foul: 0 },
+    {nameTeam:'Milan', points: 0, foul: 0 },
+    {nameTeam:'Inter', points: 0, foul: 0 },
+    {nameTeam:'Napoli', points: 0, foul: 0 }
+];
+
+console.log('soccerTeam', soccerTeam);
+
+soccerTeam.forEach((element, index) => {
+    const { nameTeam, points, foul } = element;
+    console.log(`Team ${index + 1}: ${nameTeam}, Punti: ${points}, Falli subiti: ${foul}`);
+  });
+

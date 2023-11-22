4f5acfcd0fcd938a0a53872a214e0d1b28f1c6dd
MatteoSanson <matteo.sanson@gmail.com>
2023-11-22 21:18:32 +0100

    feat: snack2

--- snack2/main.js
+++ snack2/main.js
@@ -1,16 +1,27 @@
 'use strict';
 
 const soccerTeam = [
-    {nameTeam:'Juventus', points: 0, foul: 0 },
-    {nameTeam:'Milan', points: 0, foul: 0 },
-    {nameTeam:'Inter', points: 0, foul: 0 },
-    {nameTeam:'Napoli', points: 0, foul: 0 }
+    {nameTeam:'Juventus', points: 0, fouls: 0 },
+    {nameTeam:'Milan', points: 0, fouls: 0 },
+    {nameTeam:'Inter', points: 0, fouls: 0 },
+    {nameTeam:'Napoli', points: 0, fouls: 0 }
 ];
 
-console.log('soccerTeam', soccerTeam);
+console.log('soccerTeam originario', soccerTeam);
 
 soccerTeam.forEach((element, index) => {
-    const { nameTeam, points, foul } = element;
-    console.log(`Team ${index + 1}: ${nameTeam}, Punti: ${points}, Falli subiti: ${foul}`);
-  });
+    const randomPoints = Math.floor(Math.random() * 101);
+    const randomFouls = Math.floor(Math.random() * 101);
 
+    element.points = randomPoints;
+    element.fouls = randomFouls;
+
+    console.log(`Team ${index + 1}: ${element.nameTeam}, Punti: ${randomPoints}, Falli subiti: ${randomFouls}`);
+});
+
+console.log('soccerTeam aggiornato',soccerTeam);
+
+//// nuovo array
+const newSocTeam = soccerTeam.map(({nameTeam, fouls}) => ({nameTeam, fouls}));
+
+console.log('Nuovo array', newSocTeam);

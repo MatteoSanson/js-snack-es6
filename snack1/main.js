c25782042baa344ad3fa35014af9122ba78668ba
MatteoSanson <matteo.sanson@gmail.com>
2023-11-22 15:34:58 +0100

    feat: array oggetti bikeList

--- snack1/main.js
+++ snack1/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-console.log('sto funzionando');
+const bikeList = [
+    {
+        bikeName: 'Thunder',
+        bikeWeight: '6'
+    },
+    {
+        bikeName: 'Venom',
+        bikeWeight: '7'
+    },
+    {
+        bikeName: 'Steel',
+        bikeWeight: '8'
+    },
+    {
+        bikeName: 'Franco',
+        bikeWeight: '9'
+    },
+    {
+        bikeName: 'Beppi',
+        bikeWeight: '6,5'
+    }
+]
+
+console.log(bikeList);
+
+bikeList.forEach((element, index) => {
+    console.log(`Oggetto ${index + 1} -`, element);
+    for (const key in element) {
+        if (key === 'bikeWeight') {
+            console.log(`Bici ${index + 1} - Peso: ${element[key]} kg`);
+        } else {
+            console.log(`Bici ${index + 1} - Nome: ${element[key]}`);
+        }
+    }
+});

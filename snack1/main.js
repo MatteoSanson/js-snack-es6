64b0688b43b01d262cc61a457e48525f78fa6df0
MatteoSanson <matteo.sanson@gmail.com>
2023-11-22 17:36:00 +0100

    feat: prova destructuring

--- snack1/main.js
+++ snack1/main.js
@@ -28,27 +28,42 @@ console.log(bikeList);
 const divBike = document.querySelector('.topBike');
 let lightBike = bikeList[0];
 
+// bikeList.forEach((element, index) => {
+//     console.log(`Oggetto ${index + 1}`, element);
+//     for (const key in element) {
+//          if (key === 'bikeWeight') {
+//             console.log(`Bici ${index + 1} - Peso: ${element[key]} kg`);
+//         } else {
+//             console.log(`Bici ${index + 1} - Nome: ${element[key]}`);
+//         }
+//     }
+//     if (parseFloat(element.bikeWeight.replace(',', '.')) < parseFloat(lightBike.bikeWeight.replace(',', '.'))) {
+//         lightBike = element;
+//     }
+// });
+
+// if (lightBike) {
+//     console.log(`La bici che pesa di meno è ${lightBike.bikeName} col peso di ${lightBike.bikeWeight} kg`);
+//     divBike.innerHTML = `
+//         <p><strong>Nome Bici: </strong>${lightBike.bikeName}</p>
+//         <p><strong>Peso Bici: </strong>${lightBike.bikeWeight} kg</p>
+//     `;
+// } else {
+//     console.log('Nessuna bici trovata.');
+//     divBike.innerHTML = 'Nessuna bici trovata.';
+// }
+
+////// destructuring
 bikeList.forEach((element, index) => {
-    console.log(`Oggetto ${index + 1}`, element);
-    for (const key in element) {
-         if (key === 'bikeWeight') {
-            console.log(`Bici ${index + 1} - Peso: ${element[key]} kg`);
-        } else {
-            console.log(`Bici ${index + 1} - Nome: ${element[key]}`);
-        }
-    }
-    if (parseFloat(element.bikeWeight.replace(',', '.')) < parseFloat(lightBike.bikeWeight.replace(',', '.'))) {
+    const {bikeName, bikeWeight} = element;
+
+    console.log(`Oggetto ${index + 1}`,element);
+    console.log(`Nome bici: ${bikeName}`);
+    console.log(`Peso bici: ${bikeWeight} kg`);
+
+    if (parseFloat(bikeWeight.replace(',', '.')) < parseFloat(lightBike.bikeWeight.replace(',','.'))){
         lightBike = element;
+        console.log(`la bici che pesa di meno è ${bikeName} col peso di ${bikeWeight} kg`)
     }
 });
 
-if (lightBike) {
-    console.log(`La bici che pesa di meno è ${lightBike.bikeName} col peso di ${lightBike.bikeWeight} kg`);
-    divBike.innerHTML = `
-        <p><strong>Nome Bici: </strong>${lightBike.bikeName}</p>
-        <p><strong>Peso Bici: </strong>${lightBike.bikeWeight} kg</p>
-    `;
-} else {
-    console.log('Nessuna bici trovata.');
-    divBike.innerHTML = 'Nessuna bici trovata.';
-}
